5605db75a7b80b1a2b7a16fad4598f024d423b38
Nick Zavinsky <nickvinsky@gmail.com>
2017-03-17 18:13:06 +0300

    Добавлена возможность возврата вылетевшего из игры игрока

---
+++ Assets/Code/Game/Data/PlayerInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace Code.Game.Data {
+    public struct Player {
+        public string UniKey;
+
+        public int ID;
+        public string PlayerName;
+        public PlayerColor Color;
+
+        public GameStage Stage;
+        public byte FollowersNumber;
+        public int Score;
+
+        public bool IsRegistred;
+        public bool IsReady;
+        public bool WaitingForColorUpgrade;
+
+        public int MySlotNumber;
+        public int MySlotNumberInGame;
+
+        public bool IsMyTurn;
+    }
+}
---
+++ Assets/Code/Game/Data/ReconstructionInfo.cs
@@ -0,0 +1,5 @@
+﻿namespace Code.Game.Data {
+    public class ReconstructionInfo {
+
+    }
+}
